d37de002f4aeae405c0f3f49e768bd5bfaa81e2f
Cody Dow <wodydoc@gmail.com>
2019-07-25 19:56:37 +0200

    app configuration

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
