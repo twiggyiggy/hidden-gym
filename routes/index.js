163f38b582c9c98f0636aa784a49c47598cd5fc8
Cody Dow <wodydoc@gmail.com>
2019-07-26 13:19:16 +0200

    removed links from index.hbs, modified layout.hbs so that when user is not logged in they see all the buttons, but when they are logged in, they dont see the IM NEW button, they only see the LOGOUT button

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hidden Gym' });
 });
 
 module.exports = router;
