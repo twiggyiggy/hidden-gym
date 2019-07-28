08a3435116c01d6974ed2c584ae6d820919cf174
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-07-28 12:49:02 +0200

    in index.js include condition: if user is logged in and opens the app, gets redirected straight to gyms list

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  if (req.session.currentUser) {
+    return res.redirect('/gyms');
+  }
   res.render('index', { title: 'Hidden Gym: Splash Screen' });
 });
 
