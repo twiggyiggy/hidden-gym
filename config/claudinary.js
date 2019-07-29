a0288ed4c0bbf31403f19a8a29dc1c5f3502bbbe
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-07-29 13:04:35 +0200

    create config folder for claudinary configuration

---
+++ config/claudinary.js
@@ -0,0 +1,20 @@
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+  cloud_name: 'your cloud_name',
+  api_key: 'yoyr api_key',
+  api_secret: 'your api_secret'
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'name folder',
+  allowedFormats: ['jpg', 'png']
+});
+
+const parser = multer({ storage: storage });
+
+module.exports = parser
+;
